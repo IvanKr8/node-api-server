f7dd960d8dfa2f13fadcb44ed56ae6a6803cfe72
Vania <vaniakraich@gmail.com>
2024-07-15 13:30:24 +0300

    add a other routers. Update a old requests;

--- src/service/app-service.js
+++ src/service/app-service.js
@@ -1,24 +1,28 @@
 const Product = require("../db/db_schemes");
 
-async function getAllProducts() {
+async function getProducts(filter, sortOption) {
     try {
-        const products = await Product.find();
+        let query = Product.find(filter);
+
+        if (sortOption) {
+            query = query.sort(sortOption);
+        }
+
+        const products = await query.exec();
         return products;
     } catch (error) {
-        console.error("Error retrieving products:", error);
-        throw new Error("Failed to retrieve products");
+        throw new Error(`Failed to get products: ${error.message}`);
     }
-}
+};
 
 async function getProductById(productId) {
     try {
         const product = await Product.findOne({ id: productId });
-
         return product;
     } catch (error) {
         throw new Error(`Failed to get product by id: ${error.message}`);
     }
-}
+};
 
 async function createNewProduct(reqBody) {
     try {
@@ -28,10 +32,36 @@ async function createNewProduct(reqBody) {
         console.error("Error creating product:", error);
         throw new Error("Failed to create product");
     }
-}
+};
+
+async function deleteProduct(productId) {
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(productId);
+        if (!deletedProduct) {
+            throw new Error('Product not found');
+        }
+        return deletedProduct;
+    } catch (error) {
+        throw new Error(`Failed to delete product: ${error.message}`);
+    }
+};
+
+async function updateProduct(productId, newData) {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(productId, newData, { new: true });
+        if (!updatedProduct) {
+            throw new Error('Product not found');
+        }
+        return updatedProduct;
+    } catch (error) {
+        throw new Error(`Failed to update product: ${error.message}`);
+    }
+};
 
 module.exports = {
-    getAllProducts,
+    getProducts,
     createNewProduct,
-    getProductById
+    getProductById,
+    deleteProduct,
+    updateProduct
 };
